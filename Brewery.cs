4d9aa226b44056bdbfc506a3e0c2f8e7ccc13d62
Chad <chadlada@gmail.com>
2022-01-25 21:02:42 -0500

    Added Brewery class Added Deserializer for list of brewerys

---
+++ Brewery.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace ApiClient
+{
+    public class Brewery
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string brewery_type { get; set; }
+        public string street { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string county_province { get; set; }
+        public string postal_code { get; set; }
+        public string country { get; set; }
+        public string longitude { get; set; }
+        public string latitude { get; set; }
+        public string phone { get; set; }
+        public string website_url { get; set; }
+    }
+}
+
+// //  "id": "10-barrel-brewing-co-san-diego",
+//     "name": "10 Barrel Brewing Co",
+//     "brewery_type": "large",
+//     "street": "1501 E St",
+//     "address_2": null,
+//     "address_3": null,
+//     "city": "San Diego",
+//     "state": "California",
+//     "county_province": null,
+//     "postal_code": "92101-6618",
+//     "country": "United States",
+//     "longitude": "-117.129593",
+//     "latitude": "32.714813",
+//     "phone": "6195782311",
+//     "website_url": "http://10barrel.com",
+//     "updated_at": "2021-10-23T02:24:55.243Z",
+//     "created_at": "2021-10-23T02:24:55.243Z
