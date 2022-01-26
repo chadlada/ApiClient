896b13b220dd4f3416693415e286009da2de6f9e
Chad <chadlada@gmail.com>
2022-01-25 20:05:25 -0500

    Added Client Added ResponseAsStream (Async)

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ApiClient
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var responseAsStream = await client.GetStreamAsync($"https://api.openbrewerydb.org/breweries?by_city=san_diego")
+        }
     }
-  }
 }
